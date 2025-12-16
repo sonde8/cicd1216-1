7ab9862b76c6c09b90d5560f1973c12ce76fac7f
sonde8 <sonde8@naver.com>
2025-12-16 10:22:06 +0900

    second commit

--- src/main/java/com/example/cicd1216_1/HomeController.java
+++ src/main/java/com/example/cicd1216_1/HomeController.java
@@ -2,11 +2,17 @@ package com.example.cicd1216_1;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
     @GetMapping("/hi")
     public String home() {
         return "home";
+    }
+
+    @GetMapping("/hi2")
+    public String home2() {
+        return "home2";
     }
 }
